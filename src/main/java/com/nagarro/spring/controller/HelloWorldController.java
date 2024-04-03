a82aeb472979cc096739a156c5f30570df9fb0da
Palak Bajpai <palak.bajpai01@nagarro.com>
2024-04-03 23:07:51 +0530

    hello palak

--- src/main/java/com/nagarro/spring/controller/HelloWorldController.java
+++ src/main/java/com/nagarro/spring/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, palak!";
     }
 }
